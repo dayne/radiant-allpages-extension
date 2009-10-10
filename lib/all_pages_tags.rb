02e733dfbd3ea3d29973ceab29ef634ac174dbea
Dayne Broderson <broderson@gmail.com>
2009-10-10 18:25:35 -0400

    radius tags can't have _ in them, switching from all_pages to allpages

--- lib/all_pages_tags.rb
+++ lib/all_pages_tags.rb
@@ -1,22 +1,22 @@
 module AllPagesTags
   include Radiant::Taggable
 
-  tag 'all_pages' do |tag|
+  tag 'allpages' do |tag|
     tag.expand
   end
 
   desc %{
     Usage:
-    <pre><code><r:all_pages [offset="number"] [limit="number"]
+    <pre><code><r:allpages [offset="number"] [limit="number"]
     [by="published_at|updated_at|created_at|slug|title|keywords|description"]
     [order="asc|desc"] [status="draft|reviewed|published|hidden|all"/></code></pre>
     Gives you all your pages back in a for loop
   }
-  tag 'all_pages:each' do |tag|
+  tag 'allpages:each' do |tag|
     options = children_find_options(tag)
     result = []
-    all_pages = Page.find(:all, options)
-    all.each_with_index do |item, i|
+    allpages = Page.find(:all, options)
+    allpages.each_with_index do |item, i|
       tag.locals.page = item
       result << tag.expand
     end
@@ -26,6 +26,6 @@ module AllPagesTags
   desc %{
     count of all the pages you have in the system? 
   }
-  tag 'all_pages:count' do |tag|
+  tag 'allpages:count' do |tag|
   end
 end
